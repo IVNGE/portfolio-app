7d7d3d2d6f973853ee27954eccac5f33825a6d0e
Iván González <Ivan.g.e.017@gmail.com>
2022-09-14 21:03:53 -0600

    Add resume file for download

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,6 +5,7 @@ import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import LogoNavbar from '../Assets/LogoBlanco.png'
 import { Link } from 'react-scroll'
+import pdf from '../Assets/Resume (Ivan Gonzalez).pdf'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -98,7 +99,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="/">
+                    <a className='flex justify-between items-center w-full text-gray-300' href={pdf} download>
                         Resume <BsFillPersonLinesFill size={30} />
                     </a>
                 </li>
